7e21d4a33f3d0a7730444b1abe06c64a39466b3d
Srushti Bhalshankar <srushtibhalshankar10@gmail.com>
2026-03-21 18:51:25 +0530

    Added client and event handler

---
+++ ChatServer.java
@@ -0,0 +1,41 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;        //for input-outputStream
+import java.net.ServerSocket;     //for seversocket
+import java.net.Socket;          //for socket
+import java.util.List;          //for list
+import java.util.Vector;       //for vector
+
+public class ChatServer {
+    
+    static List <ClientHandler> ClientList=new Vector<ClientHandler>();
+    public static void main(String args[])
+    {
+        try{
+             ServerSocket serversocket =new ServerSocket(8191);  //created a serversocket on port 8191
+             System.out.println("Server started successfully");
+
+             while(true)                                              //infinite loop for accepting the clients
+             {
+                 Socket socket=  serversocket.accept();               //accepts client connections
+                 System.out.println("client connected:"+socket);
+             
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+                ClientHandler client = new ClientHandler(socket, dis, dos);
+                ClientList.add(client);
+                Thread t1 =new Thread(client);
+                t1.start();
+             }
+           
+
+
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        
+}
+}
